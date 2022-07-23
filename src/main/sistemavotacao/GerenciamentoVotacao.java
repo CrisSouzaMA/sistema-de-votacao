d2426a67f97544ae49b6dbef1f92584b4f4aa29e
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-22 22:46:51 -0300

    Feat: implementa método para cadastrar pessoa candidata

--- src/main/sistemavotacao/GerenciamentoVotacao.java
+++ src/main/sistemavotacao/GerenciamentoVotacao.java
@@ -8,4 +8,21 @@ public class GerenciamentoVotacao {
   private final ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
   private final ArrayList<String> cpfComputado = new ArrayList<>();
   private int totalVotos = 0;
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    boolean ehCadastrado = false;
+    for (PessoaCandidata novoCandidato : this.pessoasCandidatas) {
+      if (novoCandidato.getNumero() == numero) {
+        ehCadastrado = true;
+        break;
+      }
+    }
+
+    if (ehCadastrado) {
+      System.out.println("Número pessoa candidata já utilizado!");
+    } else {
+      PessoaCandidata novoCandidato = new PessoaCandidata(nome, numero);
+      pessoasCandidatas.add(novoCandidato);
+    }
+  }
 }
