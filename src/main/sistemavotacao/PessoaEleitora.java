5b153bb0b5f9e84a49448a544aff57c6a5cae8a9
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-22 22:45:03 -0300

    Feat: implementa subclasse PessoaEleitora

--- src/main/sistemavotacao/PessoaEleitora.java
+++ src/main/sistemavotacao/PessoaEleitora.java
@@ -1,4 +1,28 @@
 package main.sistemavotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return this.nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
