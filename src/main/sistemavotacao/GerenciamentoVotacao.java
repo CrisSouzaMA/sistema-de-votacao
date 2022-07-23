f4d8607668a4e7553db92a5396ae8b867e1d685b
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-22 22:46:05 -0300

    Feat: implementa classe GerenciamentoVotação e seus atributos

--- src/main/sistemavotacao/GerenciamentoVotacao.java
+++ src/main/sistemavotacao/GerenciamentoVotacao.java
@@ -1,4 +1,11 @@
 package main.sistemavotacao;
 
+import java.util.ArrayList;
+
 public class GerenciamentoVotacao {
+
+  private final ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private final ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private final ArrayList<String> cpfComputado = new ArrayList<>();
+  private int totalVotos = 0;
 }
